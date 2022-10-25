eace0b2955442abe85e79603938d025367cdba76
ui0079 <ui0079el@gmail.com>
2022-10-25 18:44:44 +0900

    変換機能追加

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -6,12 +6,21 @@ function publish() {
     const userName = $('#userName').val();
     // 入力されたメッセージを取得
     const message = $('#message').val();
-    // 投稿内容を送信
-    socket.emit('sendMessageEvent', message, userName);
+
+    // メッセージを取得
+    const position = '上司'
+
+    // 自クライアントに投稿内容を送信する
+    socket.emit('sendMessageEvent', message, userName, position);
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('receiveMessageEvent', function (message, userName) {
+socket.on('receiveMyselfMessageEvent', function (message, userName, position) {
     // 画面上にメッセージを表示
-    $('#thread').prepend('<p>' + userName + 'さん：'+  message + '</p>');
+    $('#thread').prepend(`<p>${userName}さん (${position})： ${message}</p>`);
+});
+
+socket.on('receiveOtherMessageEvent', function (message, userName, position) {
+    // 画面上にメッセージを表示
+    $('#thread').prepend(`<p>${userName}さん (${position})： ${message}</p>`);
 });
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -3,15 +3,32 @@
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
     // メッセージ入力イベント（sendMessageEvent）を受信する
-    socket.on('sendMessageEvent', function (message, userName) {
+    socket.on('sendMessageEvent', function (message, userName, position) {
         if (!message || !userName) {
             return;
         }
 
-        console.log('クライアントの入力値：' + userName + message);
+        console.log(`クライアントの入力値\n  userName: ${userName}, message: ${message}`);
 
-        // 全クライアントが受信するメッセージ表示イベント（receiveMessageEvent）を送信する
-        io.sockets.emit('receiveMessageEvent', message, userName);
+        let messageMyself = message;
+
+        let messageOther = message;
+        
+        let originalList = ['どうですか', 'お世話になります'];
+        let convertList = ['どんな感じ', '元気してる'];
+        for(let i=0; i<originalList.length; i++){
+            messageOther = messageOther.replace(originalList[i], convertList[i]);
+        }
+
+        console.log(messageOther);
+        
+
+        // 全クライアントが受信するメッセージ表示イベントを送信する
+        socket.emit('receiveMyselfMessageEvent', messageMyself, userName, position);
+
+        socket.broadcast.emit('receiveOtherMessageEvent', messageOther, userName, position);
 
     });
+
+
 };
