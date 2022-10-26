ee342fe0108bd8f10662f2c3dd2c5836d89300ea
INatsukiI <e1q20019@oit.ac.jp>
2022-10-26 16:27:43 +0900

    誰に送るかの画面追加

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -14,4 +14,10 @@ router.post('/room', function (request, response, next) {
     response.render('room', { userName: request.body.userName });
 });
 
+// 誰に送るかの画面の表示
+router.post('/person', function (request, response, next) {
+    console.log('ユーザ名：' + request.body.userName);
+    response.render('person', { userName: request.body.userName });
+});
+
 module.exports = router;
