18079effc8f52ffe986e46d6d1bc3c5b0ea2cd3d
ui0079 <ui0079el@gmail.com>
2022-11-01 21:18:09 +0900

    データベースの実装

--- chatapp/sockets/index.js
+++ chatapp/sockets/index.js
@@ -1,6 +1,18 @@
 'use strict';
 
 module.exports = function (server) {
+    const sqlite3 = require("sqlite3");
+    const db = new sqlite3.Database("./test.db");
+    let data;
+
+    db.serialize(() => {
+        db.all("select * from message", (err, rows) => {
+            console.log(JSON.stringify(rows));
+        });
+        db.get("select name from users", (err, count) => {
+
+        })
+    });
 
     const socketIo = require('socket.io')(server, { wsEngine: 'ws' });
     const io = socketIo.listen(server);
