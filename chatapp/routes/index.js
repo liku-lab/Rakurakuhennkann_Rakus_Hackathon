1998fcc653e43ed68f5bf87c71a6b5baad486fcb
INatsukiI <e1q20019@oit.ac.jp>
2022-11-07 22:33:19 +0900

    誰に送るかの画面表示少し変更

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -58,13 +58,17 @@ router.post('/person', function (request, response, next) {
     promise.then(() => {
         console.log(data);
         var content;
+        var name;
+
+        (request.body.position === '上司') ? name = "AAA" : name = "ZZZ";
+        
         if (data !== undefined) {
             (request.body.position === '部下' && data.user_id === 1) ? content = data.convert_content : content = data.original_content;
             (request.body.position === '上司' && data.user_id === 2) ? content = data.convert_content : content = data.original_content;
         } else {
             content = "なし";
         }
-        response.render('person', { userName: request.body.userName, position: request.body.position, content: content });
+        response.render('person', { userName: request.body.userName, position: request.body.position, content: content, name: name });
     })
 });
 
