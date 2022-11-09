a6e7419a54112b2413a9af835f83218b2aae2e6c
ui0079 <ui0079el@gmail.com>
2022-11-09 10:27:41 +0900

    最終版

--- chatapp/public/javascripts/chat_header.js
+++ chatapp/public/javascripts/chat_header.js
@@ -2,8 +2,8 @@ const position = $('#position').val();
 var textForm = document.getElementById("chat-header");
 
 if(position == '上司'){
-  textForm.innerHTML = '田中さん';
+  textForm.innerHTML = '田中さん（部下）';
 }
 else{
-  textForm.innerHTML = '佐藤さん';
+  textForm.innerHTML = '佐藤さん（上司）';
 }
